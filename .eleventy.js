f0d1867d1a83a312ae10c323c2a40ef2fbfec8eb
Matt Henry <hey@matthenry.fyi>
2024-02-19 16:04:44 -0500

    Meh, I don't actually need vite for this

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,24 @@
-const { DateTime } = require('luxon');
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter('postDate', (dateObj) => {
+  eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  eleventyConfig.addFilter('timestamp', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat('yyyy-MM-dd');
+  eleventyConfig.addFilter("timestamp", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+  });
+
+  // eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/normalize.css/normalize.css": "css/normalize.css",
+    "src/css/main.css": "css/main.css",
   });
 
   return {
     dir: {
-      input: 'src',
-      output: 'src',
+      input: "src",
+      output: "_site",
     },
   };
 };
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-const { defineConfig } = require('vite');
+const { defineConfig } = require("vite");
+const { resolve } = require("path");
+const { globSync } = require("glob");
+
+const postHtmlPath = globSync("src/posts/**/*.html");
 
 module.exports = defineConfig({
-  root: 'src',
+  root: "src",
   clearScreen: false, // This is to show Eleventy output in the console along with Vite output
   build: {
-    outDir: '../public', // The output directory is relative to the project root, so we need to put it back one folder to work
+    outDir: "../public", // The output directory is relative to the project root, so we need to put it back one folder to work
+    rollupOptions: {
+      input: {
+        main: resolve("src", "index.html"),
+        nested: postHtmlPath,
+      },
+    },
   },
-  appType: 'mpa',
+  appType: "mpa",
 });
